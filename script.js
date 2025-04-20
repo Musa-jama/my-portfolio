6d840f116e2ef5bb75ce229eb3d749c8e9b917ad
musajama <musanuh36@gmail.com>
2025-04-20 16:50:04 -0400

    Add JavaScript for interactive project details

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    
     const toggle = document.getElementById('darkModeToggle');
     toggle.addEventListener('click', () => {
         document.body.classList.toggle('dark');
